15d6a501c9fcffeb3756922089fd0cf210257638
stefan <stefanchelaru96@gmail.com>
2020-05-27 18:53:56 +0300

    +added android check

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-import { NativeModules } from 'react-native';
-const { ReactNativeContactsSortOrder } = NativeModules;
+import {NativeModules, Platform} from 'react-native';
+
+const {ReactNativeContactsSortOrder} = NativeModules;
 
 const GIVEN_NAME = "GivenName";
 const FAMILY_NAME = "FamilyName";
 const DEFAULT = "Default";
+let getSortOrder;
 
-const getSortOrder = ReactNativeContactsSortOrder.getContactsSortOrder;
+if (Platform.OS === 'android') {
+    getSortOrder = () => FAMILY_NAME
+} else {
+    getSortOrder = ReactNativeContactsSortOrder.getContactsSortOrder;
+}
 
 export default {
     GIVEN_NAME,
