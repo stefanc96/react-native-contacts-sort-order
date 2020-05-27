5a3500769f9e22a83a2f39bccef8096662b2f0da
stefan <stefanchelaru96@gmail.com>
2020-05-27 10:12:00 +0300

    first commit

---
+++ ios/ReactNativeContactsSortOrder.m
@@ -0,0 +1,31 @@
+#import "ReactNativeContactsSortOrder.h"
+@import Contacts;
+
+@implementation ReactNativeContactsSortOrder
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(getContactsSortOrder: callback:(RCTResponseSenderBlock)callback)
+{
+    CNContactSortOrder sortOrder = CNContactsUserDefaults.sharedDefaults.sortOrder;
+    
+    NSString *const GIVEN_NAME = @"GivenName";
+    NSString *const FAMILY_NAME = @"FamilyName";
+    NSString *const DEFAULT = @"Default";
+    NSString *order;
+    
+    switch (sortOrder) {
+      case CNContactSortOrderGivenName:
+        order = GIVEN_NAME;
+        break;
+      case CNContactSortOrderFamilyName:
+        order = FAMILY_NAME;
+        break;
+      default:
+        order = DEFAULT;
+        break;
+    }
+    callback(@[[order]]);
+}
+
+@end
